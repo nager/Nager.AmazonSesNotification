7ac8ceb99ea2eea3b0eae05bb875c459f5f6f6dd
Tino Hager <tino.hager@nager.at>
2019-08-09 16:01:01 +0200

    Add other test cases

--- src/Nager.AmazonSesNotification.UnitTest/ParsingTest.cs
+++ src/Nager.AmazonSesNotification.UnitTest/ParsingTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nager.AmazonSesNotification.Model;
+using Newtonsoft.Json;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -8,22 +10,33 @@ namespace Nager.AmazonSesNotification.UnitTest
     public class ParsingTest
     {
         [TestMethod]
-        [DeploymentItem(@"Notifications\Received.json")]
+        [DeploymentItem(@"Notifications\Notifications.json")]
+        [DeploymentItem(@"Notifications\RawMessageDelivery\Received.json")]
+        [DeploymentItem(@"Notifications\RawMessageDelivery\Bounce.json")]
+        [DeploymentItem(@"Notifications\RawMessageDelivery\Delivery.json")]
+        [DeploymentItem(@"Notifications\RawMessageDelivery\Complaint.json")]
         public async Task ProcessReceivedTest()
         {
-            var json = File.ReadAllText(@"Notifications\Received.json");
-            var item = await new NotificationProcessor().ProcessNotificationAsync(json);
-
-            Assert.IsNotNull(item);
+            await this.CheckAsync("Received.json");
+            await this.CheckAsync("Bounce.json");
+            await this.CheckAsync("Delivery.json");
+            await this.CheckAsync("Complaint.json");
         }
 
-        [TestMethod]
-        [DeploymentItem(@"Notifications\RawMessageDelivery\Received.json")]
-        public async Task ProcessRawReceivedTest()
+        private async Task CheckAsync(string file)
         {
-            var json = File.ReadAllText(@"Notifications\RawMessageDelivery\Received.json");
-            var item = await new NotificationProcessor().ProcessNotificationAsync(json);
+            var jsonNotification = File.ReadAllText(@"Notifications\Notification.json");
+            var jsonDetails = File.ReadAllText($@"Notifications\RawMessageDelivery\{file}");
 
+            var minimized = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(jsonDetails), Formatting.None);
+            var notification = JsonConvert.DeserializeObject<SnsMessage>(jsonNotification);
+            notification.Message = minimized;
+            var json = JsonConvert.SerializeObject(notification, Formatting.Indented);
+
+            var item = await new NotificationProcessor().ProcessNotificationAsync(json);
+            Assert.IsNotNull(item);
+
+            item = await new NotificationProcessor().ProcessNotificationAsync(jsonDetails);
             Assert.IsNotNull(item);
         }
     }
