7fd106e2bd282f8d536de880ccdf37b88f40cea6
Tino Hager <tino.hager@nager.at>
2019-08-11 10:28:19 +0200

    Add demo website

---
+++ src/Nager.AmazonSesNotification.WebhookDemo/Controllers/SesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nager.AmazonSesNotification.Model;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Nager.AmazonSesNotification.WebhookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SesController : ControllerBase
+    {
+        [Route("SesNotification")]
+        [HttpPost]
+        public async Task<IActionResult> SesNotificationAsync()
+        {
+            var body = string.Empty;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+
+            var notificationProcessor = new NotificationProcessor();
+            var result = await notificationProcessor.ProcessNotificationAsync(body);
+
+            //Your processing logic...
+            if (result.NotificationType == "Bounce")
+            {
+                var webhook = result as EmailBounceWebhook;
+                if (webhook.Bounce.BounceType == "Hardbounce")
+                {
+
+                }
+            }
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
